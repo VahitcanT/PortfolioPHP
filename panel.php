bb35701ef34563f20901e7406d1b2e0d14ee592c
Emre Özincegedik <emreozincegedik@gmail.com>
2020-12-13 01:16:11 +0300

    göze güzel gözükmesi amacıyla değiştirildi

--- panel.php
+++ panel.php
@@ -26,57 +26,57 @@ $message="";
 
 <body>
   <section class="bilgi">
-<?php include 'navbar.php';?>
-<form name="LogInForm" action="includes/updateContact.inc.php" method="POST">
-<h1>Contact details</h1>
-            <p>Name and Surname</p>
-            <input type="text" name="name" id="name" placeholder="Name and Surname">
-            <p>Number</p>
-            <input type="text" name="number" id="number" placeholder="Number">
-            <p>Email</p>
-            <input type="text" name="email" id="email" placeholder="Email">
-            <input class="buton" type="submit" name="login" onclick="return valMessage('errormsg','name','number','email')">
+  <?php include 'navbar.php';?>
+  <form name="LogInForm" action="includes/updateContact.inc.php" method="POST">
+    <h1>Contact details</h1>
+    <p>Name and Surname</p>
+    <input type="text" name="name" id="name" placeholder="Name and Surname">
+    <p>Number</p>
+    <input type="text" name="number" id="number" placeholder="Number">
+    <p>Email</p>
+    <input type="text" name="email" id="email" placeholder="Email">
+    <input class="buton" type="submit" name="login" onclick="return valMessage('errormsg','name','number','email')">
 
-            <?php
-            if (isset($_GET["update"])) {
-              
-              if ($_GET["update"]=="success") {
-                $message="Contact detail updated succesfully";
-              }
-              if ($_GET["update"]=="failed") {
-                $message=$_GET["error"];
-              }
-            }
-            
-            ?>
-            <p id="errormsg"><?php echo($message);?></p>
-        </form>
+    <?php
+    if (isset($_GET["update"])) {
+      
+      if ($_GET["update"]=="success") {
+        $message="Contact detail updated succesfully";
+      }
+      if ($_GET["update"]=="failed") {
+        $message=$_GET["error"];
+      }
+    }
+    
+    ?>
+    <p id="errormsg"><?php echo($message);?></p>
+  </form>
 
-<table>
-  <tr>
-    <th>Messages</th>
-    <th>Name</th>
-    <th>Email</th>
-    <th>Comment</th>
-  </tr>
-  <?php
-  include 'includes/autoloader.inc.php';
-  
-  $messageObj=new Message("","","");
-  
-  $messages=$messageObj->displayMessages();
+  <table>
+    <tr>
+      <th>Messages</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Comment</th>
+    </tr>
+    <?php
+      include 'includes/autoloader.inc.php';
+      
+      $messageObj=new Message("","","");
+      
+      $messages=$messageObj->displayMessages();
 
-  foreach ($messages as $key => $value) {
-    echo("<tr>");
-    echo("<td>".$value["id"]."</td>");
-    echo("<td>".$value["name"]."</td>");
-    echo("<td>".$value["email"]."</td>");
-    echo("<td>".$value["comment"]."</td>");
-    echo("</tr>");
-  }
-  
-  ?>
-</table>
+      foreach ($messages as $key => $value) {
+        echo("<tr>");
+        echo("<td>".$value["id"]."</td>");
+        echo("<td>".$value["name"]."</td>");
+        echo("<td>".$value["email"]."</td>");
+        echo("<td>".$value["comment"]."</td>");
+        echo("</tr>");
+      }
+    
+    ?>
+  </table>
 </section>
 <script src="js/validation.js">
 
